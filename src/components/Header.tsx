b3aa67b72be84daadac513761d2fca8f3674736c
Moong <tmdals9154@gmail.com>
2023-12-11 11:52:33 +0900

    ✨ [Feat] Header 반응형 레이아웃 완성

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,45 +2,47 @@ import React from 'react';
 import styled from 'styled-components';
 
 const Head = styled.nav`
-	background-color: black;
-	width: 100%;
-	color: white;
 	display: flex;
 	justify-content: space-between;
+	padding-left: 3%;
+	padding-right: 3%;
+	padding-top: 10px;
+	background-color: skyblue;
 `;
 
-const Title = styled.h1`
-	align-items: center;
-	text-align: center;
+const TitleWrapper = styled.div``;
+
+const Title = styled.a`
 	font-size: 3rem;
 `;
 
-const Menu = styled.button`
-	margin: auto auto;
-	padding: 20px 20px;
-	height: auto;
-	margin: 10px;
-`;
-
-const TT = styled.div`
-	margin: auto 0;
+const MenuWrapper = styled.div`
 	display: flex;
 	justify-content: space-between;
-	margin-right: 30px;
+	align-items: center;
+	min-width: 333px;
+`;
+
+const Menu = styled.button`
+	font-size: 1.5rem;
+	min-width: 90px;
+	background-color: yellow;
+	margin: 0 15px;
+	padding: 10px 30px;
 `;
 
 export default function Header(): JSX.Element {
 	return (
-		<div>
-			<Head>
+		<Head>
+			<TitleWrapper>
 				<Title>SM`S</Title>
-				<TT>
-					<Menu>첫번째</Menu>
-					<Menu>두번째</Menu>
-					<Menu>세번째</Menu>
-					<Menu>네번째</Menu>
-				</TT>
-			</Head>
-		</div>
+			</TitleWrapper>
+			<MenuWrapper>
+				<Menu>첫번째</Menu>
+				<Menu>두번째</Menu>
+				<Menu>세번째</Menu>
+				<Menu>네번째</Menu>
+			</MenuWrapper>
+		</Head>
 	);
 }
