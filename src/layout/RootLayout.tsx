9d140ad63b2f3e5e3198e6fdc5a5fcb65d7672d5
Moong <tmdals9154@gmail.com>
2023-12-14 16:27:57 +0900

    🔥 [Remove] 이사갈 준비 완료!

--- src/layout/RootLayout.tsx
+++ src/layout/RootLayout.tsx
@@ -1,7 +1,7 @@
+import Header from '@/components/Header';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import Footer from '../components/Footer';
-import Header from '../components/Header';
 
 const Section = styled.section`
 	position: absolute;
